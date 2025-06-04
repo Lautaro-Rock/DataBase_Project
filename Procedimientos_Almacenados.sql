c5ef4ef3ee266cfc660d6ea04e797f6853c9e2e8
Lautaro Maldonado <lautylty@hotmail.com>
2025-06-04 20:53:52 -0300

    agregando funcionalidades CRUD a empleados

--- Procedimientos_Almacenados.sql
+++ Procedimientos_Almacenados.sql
@@ -1,7 +1,5 @@
 Use GESTION_STOCK;
-
 GO
-
 --Procedimiento almacenado para agregar producto a la tabla Productos
 CREATE PROCEDURE sp_AgregarProducto
     @NombreProducto NVARCHAR(100),
@@ -54,7 +52,7 @@ END;
 
 GO
 
---Procedimiento almacenado para elminiar un producto de la tabla Productos
+--Procedimiento almacenado para eliminiar un producto de la tabla Productos
 CREATE PROCEDURE sp_EliminarProducto
     @IdProducto INT
 AS
@@ -87,5 +85,49 @@ BEGIN
         THROW;
     END CATCH
 END;
-
+GO
+--EMPLEADOS
+--Procedimiento almacenado para agregar empleados a la tabla Empleados
+Create Procedure sp_AgregarEmpleados
+@Nombre NVARCHAR(100),
+@Apellido NVARCHAR(100),
+@Telefono NVARCHAR(20),
+@Email NVARCHAR(100),
+@IdTipoRol INT,
+@Activo BIT
+AS
+BEGIN
+BEGIN TRY
+BEGIN TRANSACTION
+INSERT INTO Empleados(Nombre,Apellido,Telefono,Email,IdTipoRol,Activo)
+VALUES(@Nombre,@Apellido,@Telefono,@Email,@IdTipoRol,@Activo);
+COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+ROLLBACK
+THROW;
+END CATCH
+END
+GO
+--Procedimiento almanceado para editar empleados de la tabla empleados
+Create Procedure sp_EditarEmpleados
+@IdEmpleado INT,
+@Nombre NVARCHAR(100),
+@Apellido NVARCHAR(100),
+@Telefono NVARCHAR(20),
+@Email NVARCHAR(100),
+@IdTipoRol INT,
+@Activo BIT
+AS
+BEGIN
+BEGIN TRY
+BEGIN TRANSACTION
+UPDATE Empleados SET Nombre=@Nombre, Apellido=@Apellido,Telefono=@Telefono,Email=@Email,IdTipoRol=@IdTipoRol,Activo=@Activo WHERE @IdEmpleado=@IdEmpleado;
+COMMIT TRANSACTION
+END TRY
+BEGIN CATCH
+ROLLBACK
+THROW
+END CATCH
+END
 GO
