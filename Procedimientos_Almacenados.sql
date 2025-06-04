7e81da3dcd24016b95cdb8df82e07e7bdea9849a
Jerez-Bernardo-02 <jerez.bernardo.02@gmail.com>
2025-06-04 01:06:46 -0300

    Tabla "Productos" - Procedimientos Almacenados
    Procedimientos almacenados para leer y eliminar productos

--- Procedimientos_Almacenados.sql
+++ Procedimientos_Almacenados.sql
@@ -1,52 +1,91 @@
 Use GESTION_STOCK;
-go
---Almacenamiento procesado para agregar producto a la tabla Productos
-Create Procedure sp_AgregarProducto
-@NombreProducto NVARCHAR(100),
-@IdCategoria INT,
-@Stock INT,
-@StockMinimo INT,
-@PrecioUnitario Decimal(10,2),
-@IdProveedor INT,
-@Estado BIT
-AS
-BEGIN
-BEGIN TRY
-BEGIN TRANSACTION
-INSERT INTO Productos(NombreProducto,IdCategoria,Stock,StockMinimo,PrecioUnitario,IdProveedor,Estado)
-Values(@NombreProducto,@IdCategoria,@Stock,@StockMinimo,@PrecioUnitario,@IdProveedor,@Estado);
-COMMIT TRANSACTION
-END TRY
-BEGIN CATCH
-ROLLBACK TRANSACTION;
-THROW;
-END CATCH
-END
+
 GO
---Almacenamiento procesado para editar producto en la tabla Productos
-Create Procedure sp_EditarProducto
-@IdProducto INT,
-@NombreProducto NVARCHAR(100),
-@IdCategoria INT,
-@Stock INT,
-@StockMinimo INT,
-@PrecioUnitario Decimal(10,2),
-@IdProveedor INT,
-@Estado BIT
+
+--Procedimiento almacenado para agregar producto a la tabla Productos
+CREATE PROCEDURE sp_AgregarProducto
+    @NombreProducto NVARCHAR(100),
+    @IdCategoria INT,
+    @Stock INT,
+    @StockMinimo INT,
+    @PrecioUnitario Decimal(10,2),
+    @IdProveedor INT,
+    @Estado BIT
 AS
 BEGIN
-BEGIN TRY
-BEGIN TRANSACTION
-UPDATE Productos SET NombreProducto=@NombreProducto, IdCategoria=@IdCategoria, Stock=@Stock,StockMinimo=@StockMinimo,PrecioUnitario=@PrecioUnitario,IdProveedor=@IdProveedor,Estado=@Estado WHERE IdProducto=@IdProducto;
-COMMIT TRANSACTION
-END TRY
-BEGIN CATCH
-ROLLBACK TRANSACTION;
-THROW;
-END CATCH
-END
+    BEGIN TRY
+        BEGIN TRANSACTION
+            INSERT INTO Productos(NombreProducto,IdCategoria,Stock,StockMinimo,PrecioUnitario,IdProveedor,Estado)
+            Values(@NombreProducto,@IdCategoria,@Stock,@StockMinimo,@PrecioUnitario,@IdProveedor,@Estado);
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END;
 
+GO
 
+--Procedimiento almacenado para editar producto en la tabla Productos
+CREATE PROCEDURE sp_EditarProducto
+    @IdProducto INT,
+    @NombreProducto NVARCHAR(100),
+    @IdCategoria INT,
+    @Stock INT,
+    @StockMinimo INT,
+    @PrecioUnitario Decimal(10,2),
+    @IdProveedor INT,
+    @Estado BIT
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION
+            UPDATE Productos 
+            SET NombreProducto=@NombreProducto, IdCategoria=@IdCategoria, Stock=@Stock,StockMinimo=@StockMinimo,PrecioUnitario=@PrecioUnitario,IdProveedor=@IdProveedor,Estado=@Estado 
+            WHERE IdProducto=@IdProducto;
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END;
 
+GO
 
+--Procedimiento almacenado para elminiar un producto de la tabla Productos
+CREATE PROCEDURE sp_EliminarProducto
+    @IdProducto INT
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION
+            DELETE FROM Productos
+            WHERE IdProducto = @IdProducto;
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END;
 
+GO
+
+---Procedimiento almacenado para listar los productos de la tabla Productos
+CREATE PROCEDURE sp_ListarProductos
+AS
+BEGIN
+    BEGIN TRY
+        BEGIN TRANSACTION
+            SELECT * FROM Productos
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END;
+
+GO
