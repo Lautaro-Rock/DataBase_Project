30b6eba98977f39c8d46e72a1f56c6e5abd3b98b
Lautaro Maldonado <lautylty@hotmail.com>
2025-06-09 00:33:37 -0300

    trabjando en la tabla alertas

--- Triggers_DataBase.sql
+++ Triggers_DataBase.sql
@@ -13,7 +13,7 @@ BEGIN
     END
 END;
 GO
-CREATE TRIGGER trg_StockMinimo_Producto
+CREATE TRIGGER trg_StockMinimoAlerta_Producto
 ON Productos
 AFTER UPDATE
 AS 
@@ -24,6 +24,53 @@ BEGIN
  ROLLBACK TRANSACTION
  RETURN;
 END
+END
+GO
+CREATE TRIGGER trg_AlertaStockCeroNew
+ON Productos
+AFTER UPDATE
+AS
+BEGIN
+    INSERT INTO AlertaStock (IdProducto, FechaAlerta, Descripcion)
+    SELECT 
+        i.IdProducto,
+        GETDATE(),
+        'EL STOCK ES IGUAL A 0, REPONER'
+    FROM inserted i
+    WHERE i.Stock = 0;
+END
+GO
+CREATE TRIGGER trg_sumaStock
+ON Productos
+AFTER UPDATE
+AS
+BEGIN
+INSERT INTO AlertaStock(IdProducto,FechaAlerta,Descripcion)
+Select
+I.IdProducto,
+GETDATE(),
+'EL STOCK DEL PRODUCTO FUE REPUESTO'
+FROM inserted AS I
+INNER JOIN deleted AS D
+ON I.IdProducto=D.IdProducto
+Where I.Stock>D.Stock
+END
+GO
+CREATE TRIGGER trg_restaStock
+ON Productos
+AFTER UPDATE
+AS
+BEGIN
+INSERT INTO AlertaStock(IdProducto,FechaAlerta,Descripcion)
+Select 
+I.IdProducto,
+GETDATE(),
+'EL STOCK DEL PRODUCTO SE HA REDUCIDO'
+FROM 
+inserted AS I
+INNER JOIN deleted AS D
+ON I.IdProducto=D.IdProducto
+Where I.Stock<D.Stock
 END
 GO
 
