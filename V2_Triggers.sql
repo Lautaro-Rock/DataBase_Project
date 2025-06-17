ff05b93c8ec73cfe3446e48233c5a4e087a38fd7
Jerez-Bernardo-02 <jerez.bernardo.02@gmail.com>
2025-06-17 11:13:10 -0300

    feature_triggers
    Se agregaron los siguientes Triggers:
    
    - Generar alertas de manera automática cuando se alcance el stockMinimo
    - Baja logica de un producto en caso de que se quede sin stock
    - Se quitaron triggers redundantes, ya que las validaciones se resuelven en los Procedimientos Almacenados

--- V2_Triggers.sql
+++ V2_Triggers.sql
@@ -36,68 +36,51 @@ END
 
 GO
 
-CREATE TRIGGER trg_StockMinimoAlerta_Producto
+CREATE TRIGGER tr_Generar_Alerta
 ON Productos
 AFTER UPDATE
-AS 
+AS
 BEGIN
-    IF EXISTS (SELECT * FROM inserted WHERE Stock<StockMinimo) BEGIN
-        RAISERROR('EL STOCK ES MENOS QUE EL MINIMO PERMITIDO, NO SE PUEDE DESCONTAR ESA CANTIDAD', 16, 1);
-        ROLLBACK TRANSACTION
-        RETURN;
+    DECLARE @idProducto INT
+    DECLARE @stock INT
+    DECLARE @stockMinimo INT
+
+    SELECT
+        @idProducto = i.IdProducto,
+        @stock = i.Stock
+    FROM inserted i;
+
+    SELECT
+        @stockMinimo = StockMinimo
+    FROM Productos;
+
+    IF @stock <= @stockMinimo BEGIN
+        -- EN CASO DE QUE EL STOCK SEA MENOR O IGUAL AL STOCK MINIMO, AGREGO LA ALERTA
+        -- SE CONSIDERA EL = YA QUE SE PEUDE VENDER UNA CANTIDAD DE PRODUCTO IGUAL AL STOCK MINIMO
+        INSERT INTO AlertaStock(IdProducto, FechaAlerta)
+        VALUES(@idProducto, GETDATE());
+    END
+END;
+
+GO
+
+CREATE TRIGGER tr_Producto_Sin_Stock
+ON Productos
+AFTER UPDATE
+AS
+BEGIN
+    DECLARE @idProducto INT
+    DECLARE @stock INT
+
+    SELECT
+        @idProducto = i.IdProducto,
+        @stock = i.Stock
+    FROM inserted i;
+    
+    IF @stock = 0 BEGIN
+        -- BAJA LOGICA DEL PRODUCTO
+        UPDATE Productos
+        SET Estado = 0
+        WHERE IdProducto = @idProducto
     END
 END
-
-GO
-
-CREATE TRIGGER trg_AlertaStockCeroNew
-ON Productos
-AFTER UPDATE
-AS
-BEGIN
-    INSERT INTO AlertaStock (IdProducto, FechaAlerta, Descripcion)
-    SELECT 
-        i.IdProducto,
-        GETDATE(),
-        'EL STOCK ES IGUAL A 0, REPONER'
-    FROM inserted i
-    WHERE i.Stock = 0;
-END
-
-GO
-
-CREATE TRIGGER trg_sumaStock
-ON Productos
-AFTER UPDATE
-AS
-BEGIN
-    INSERT INTO AlertaStock(IdProducto,FechaAlerta,Descripcion)
-    Select
-    I.IdProducto,
-    GETDATE(),
-    'EL STOCK DEL PRODUCTO FUE REPUESTO'
-    FROM inserted AS I
-    INNER JOIN deleted AS D ON I.IdProducto=D.IdProducto
-    Where I.Stock>D.Stock
-END
-
-GO
-
-CREATE TRIGGER trg_restaStock
-ON Productos
-AFTER UPDATE
-AS
-BEGIN
-    INSERT INTO AlertaStock(IdProducto,FechaAlerta,Descripcion)
-    Select 
-        I.IdProducto,
-        GETDATE(),
-        'EL STOCK DEL PRODUCTO SE HA REDUCIDO'
-    FROM inserted AS I
-    INNER JOIN deleted AS D
-    ON I.IdProducto = D.IdProducto
-    Where I.Stock<D.Stock
-END
-
-GO
-
